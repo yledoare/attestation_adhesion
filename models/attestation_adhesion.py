cdd21113950080a61f9e730f4ed2ff5e5a12b343
Yann Le Doaré <yann@linuxconsole.org>
2025-10-15 22:17:59 +0200

    Fix1

---
+++ models/attestation_adhesion.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+from odoo.http import Response, request
+from odoo.exceptions import UserError
+from io import BytesIO
+import base64
+import io
+import datetime
+from datetime import datetime
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+class AttestationAdhesion(models.Model):
+    _name = 'attestation.adhesion'
+    _inherit = [
+            'mail.thread',
+            'mail.activity.mixin',
+            'image.mixin',
+            ]
+    _description = 'Adhesions'
+
+    def action_send_mail(self):
+      template = self.env.ref('attestation_adhesion.mail_template_receipt')
+      if template:
+        template.send_mail(self.id, force_send=True)
+      else:
+        raise UserError("Mail Template not found. Please check the template.")
+    def send_email_with_pdf_attach(self):
+      report_pdf = request.env[ "ir.actions.report" ]._render_qweb_pdf( "attestation_adhesion.attestation_adhesion_report", [self.id])
+      pdf_base64 = base64.b64encode(report_pdf[0])
+      attachment_values = {
+        'name': _("Rent receipt") + ".pdf",
+        'type': 'binary',
+        'datas': pdf_base64,
+        'mimetype': 'application/pdf',
+      }
+      attachment = self.env['ir.attachment'].create(attachment_values)
+      ir_values = {
+            'name': 'Rent receipt Report',
+            'type': 'binary',
+            'res_model': 'attestation.adhesion',
+            }
+      email_template = self.env.ref('attestation_adhesion.mail_template_receipt')
+      email_template.attachment_ids = [(4, attachment.id)]
+
+      if email_template:
+            email_template.send_mail(self.id)
+            email_template.attachment_ids = [(5, 0, 0)]
+
+    customer_id = fields.Many2one('res.partner', string='Customer')
+    amount = fields.Float('Amount')
+    payment_day = fields.Integer('Payment day')
+
+    name_of_customer = fields.Char(
+            string='Customer Name',
+            related='customer_id.name')
+    email_of_customer = fields.Char(
+            string='Customer Email',
+            related='customer_id.email')
